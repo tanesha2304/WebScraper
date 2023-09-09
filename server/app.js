2333a837462a04c37dbf49b6037e1d4a21c22483
tanesha2304 <mradulsingh524.mst@gmail.com>
2023-09-09 23:59:25 +0530

    [ADDED] web scraper project

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const PORT = 8000;
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+const fs = require("fs");
+
+const scrapeDynamicWebpage = require("./scrapers/dynamincSiteScraper");
+const scrapeStaticWebpage = require("./scrapers/staticSiteScraper");
+// const data = require("./data/itemsData.json");
+const data = require("./data/telemartData.json");
+
+var folder = "./data";
+
+if (!fs.existsSync(folder)) {
+ fs.mkdirSync(folder);
+ console.log("Folder Created Successfully.");
+}
+app.get("/", function (req, res) {
+ res.send("Happy web scraping");
+});
+
+app.get("/items", function (req, res) {
+ res.json(data);
+});
+
+app.listen(PORT, () => {
+ console.log("Server running on port " + PORT);
+});
+
+// scrapeDynamicWebpage();
+scrapeStaticWebpage();
+scrapeDynamicWebpage();
